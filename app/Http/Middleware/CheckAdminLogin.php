9c0af9ed7cf57594fbbc52db143187884a959fa1
linlinwang <907476019@qq.com>
2018-01-31 17:58:24 +0800

    Laravel-book

---
+++ app/Http/Middleware/CheckAdminLogin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckAdminLogin
+{
+    /**
+     * Run the request filter.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $admin = $request->session()->get('admin', '');
+		
+		//if($request->session()->has('admin')){
+        if($admin == '') {
+          return redirect('/admin/login');
+        }
+
+        return $next($request);
+    }
+
+}
